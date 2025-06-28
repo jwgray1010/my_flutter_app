6d7317e4bfbb1e1d0ab9abbe8546e5d4868866e6
John Gray <your@email.com>
2025-06-28 14:01:04 -0500

    Add the NDK version to the build.gradle.tks

--- my_flutter_app/android/app/build.gradle.kts
+++ my_flutter_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.unsaid.app" // <-- Update to match your package name
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
