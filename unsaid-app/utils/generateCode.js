55907d201910c5f4d087c9d326b4cff3af90e874
John Gray <your@email.com>
2025-06-28 17:48:05 -0500

    Update codemagic

--- unsaid-app/utils/generateCode.js
+++
@@ -1,5 +0,0 @@
-export const generateInviteCode = (length = 5) => {
-  const prefix = 'UNSD-';
-  const random = Math.random().toString(36).substring(2, 2 + length).toUpperCase();
-  return `${prefix}${random}`;
-};
